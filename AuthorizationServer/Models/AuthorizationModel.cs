b18698801ba14131281ce1c856cbef715b8d4619
kevin_trouw <kevintrouw@home.nl>
2020-04-16 11:20:53 +0200

    Deleted SelectedListItem

--- AuthorizationServer/Models/AuthorizationModel.cs
+++ AuthorizationServer/Models/AuthorizationModel.cs
@@ -74,7 +74,6 @@ namespace AuthorizationServer.Models
     {
         public int PermissionID { get; set; }
         public string PermissionName { get; set; }
-        public IEnumerable<SelectListItem> Permissions { get; set; }
         public int RoleID { get; set; }
         public string Description { get; set; }
     }
